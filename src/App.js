d369f66e90e97c74adb6318e561b0a8610173b4d
Zahid Ghafoor <zahidghafoor40@gmail.com>
2022-03-23 14:58:53 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./App.css"
+import Header from './Screens/Header/Header'
 import NavBar from './Screens/NavBar/NavBar'
 
 const App = () => {
   return (
   
   <>
-  <NavBar/>
+  {/* <NavBar/> */}
+  <Header/>
   </>
   )
 }
--- src/Screens/NavBar/NavBar.jsx
+++ src/Screens/NavBar/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./NavBar.scss"
+
+
+import logo from "../../Assets/main.png"
 
 const NavBar = () => {
   return (
     <div className="navbar__container">
-        <h1>dklsjz</h1>
+        <div className="logo">
+          <img src={logo} alt="" className="main__logo" />
+        </div>
+
+        <div className="tabs">
+        <NavLink className="links" to="/">Home</NavLink>
+        <NavLink className="links" to="/">About Us</NavLink>
+        <NavLink className="links" to="/">Our Businesses</NavLink>
+        <NavLink className="links" to="/">Media</NavLink>
+        <NavLink className="links" to="/">Careers</NavLink>
+        <NavLink className="links" to="/">Contact</NavLink>
+        </div>
     </div>
   )
 }
