2957e06356ca9955ed26bfbb79fcd4458896826b
Zahid Ghafoor <zahidghafoor40@gmail.com>
2022-03-27 11:58:53 -0700

    gallery

--- src/Screens/Gallery/Gallery.jsx
+++ src/Screens/Gallery/Gallery.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./Gallery.scss"
+
 
 const Gallery = () => {
     return (
        <dvi className="gallery__container">
-           
+           <div className="heading">Gallery</div>
+
+
        </dvi>
     )
 }
